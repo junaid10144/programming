1a01bda08f6d3f8ed3e38263e928b08e08b4ff43
Junaid <junaid.saleem.10144@gmail.com>
2023-08-22 15:05:27 +0500

    add angular

---
+++ Angular/angular-course/src/app/first-comp/first-comp.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-first-comp',
+  templateUrl: './first-comp.component.html',
+  styleUrls: ['./first-comp.component.scss']
+})
+export class FirstCompComponent {
+
+  name: string = '';
+  email: string = '';
+  message: string = '';
+  isSubmitted: boolean = false;
+
+  messages: Array<any> = []
+
+  onSubmit(): void {
+    this.isSubmitted = true
+    this.messages.push({
+      'name': this.name,
+      'email': this.email,
+      'message': this.message
+    })
+
+    console.log(this.messages)
+  }
+
+  deleteMessage(index: number): void {
+    this.messages.splice(index, 1)
+  }
+
+}
